20c520735de8cc080ec943651b356817e4d9fc81
GulsenZalova <zalova0202@gmail.com>
2021-12-22 01:26:53 +0400

    JS Giris

---
+++ Works/Javascript/app.js
@@ -0,0 +1,90 @@
+let a=10
+let b=15
+let c=a+b
+console.log(c)
+
+let x=20
+let y=30
+let z=x-y
+console.log(z)
+
+let e=10
+let g=20
+let d=c*b
+console.log(d)
+
+a=20
+b=5
+c=a-b
+console.log(c)
+
+a=20
+x=12
+c=a%x
+console.log(c)
+
+a=10
+a++
+a++
+a--
+a++
+a++
+a--
+a++
+console.log(a)
+
+a=5
+b="5"
+c=true
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+
+a="sun"
+b="flowers"
+c=a+b
+console.log(c)
+
+a="sun"
+b=null
+c=a+b
+console.log(c)
+
+a="sun"
+b=5
+c=a+b
+console.log(c)
+
+a="sun"
+b=true
+c=a+b
+console.log(c)
+
+a=5
+b=-5
+c=a+b
+console.log(c)
+
+a=5
+b=true
+c=a+b
+console.log(c)
+
+a=5
+b=false
+c=a+b
+console.log(c)
+
+a=5
+b=null
+c=a+b
+console.log(c)
+
+console.log(8)
+console.log(String(8))
+
+console.log(Boolean(1))
+console.log(Number("salam"))
+
+
