41f22bcf7fbd4879cf67432ef3b87cc6ae753d5f
GulsenZalova <zalova0202@gmail.com>
2021-12-23 03:21:36 +0400

    daxil edilen melumatin ekranda gorunmesini yoxladim

---
+++ Works/Javascript/condation.js
@@ -0,0 +1,15 @@
+let imtahanbali=120
+
+if(imtahanbali<50){
+    alert("Ugursuz netice")
+}
+if((imtahanbali>50)&&(imtahanbali<80)){
+    alert("Kafi")
+}
+ if((imtahanbali>80)&&(imtahanbali<100)){
+     alert("Ugurlu netice")
+ }
+
+ if((imtahanbali<0)||(imtahanbali>100)){
+     alert("Yalnis melumat")
+ }
